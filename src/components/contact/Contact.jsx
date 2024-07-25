09cd71cd883b5017917b5627b88a9807f34f2cda
Nabeela-rafi <74830541+Nabeela-rafi@users.noreply.github.com>
2024-07-25 02:25:59 +0100

    Update Contact.jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -27,7 +27,7 @@ const Contact = () => {
                     <div className="contact__card">
                         <i className="bx bx-mail-send contact__card-icon"></i>
                         <h3 className="contact__card-title">Email</h3>
-                        <span className="contact__card-data">nabeela1492000@gmail.com</span>
+                        <span className="contact__card-data">user@gmail.com</span>
                         <a href="mailto:nabeela1492000@gmail.com" className="contact__button">Write me <i className="bx bx-right-arrow-alt contact__button-icon"></i></a>
                     </div>
 
